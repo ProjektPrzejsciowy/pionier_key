185fd153cf0afef91cff9ac4f246a58479012999
Kamil <kamil.14x@gmail.com>
2016-12-19 21:45:01 +0100

    Poprawa obsługi klawiatury + drobiazgi
    Uruchomienie nołda wymaga praw administratora (lokalnie polecam 'sudo su' -> wtedy trzeba dodać rootowi ścieżkę do naszego catkina i wszystkie setup.bash bo to tak jakby zmiana użytkownika), na dockerze przetestuję potem ale powinno śmigać.

--- src/PionierKeyWywrotowiec.cpp
+++ src/PionierKeyWywrotowiec.cpp
@@ -9,73 +9,72 @@
 #include <stdio.h>
 #include <sstream>
 #include <unistd.h>
+#include <string>
+#include <iostream>
+#include "../inc/keyboard.h"
+#include <SDL/SDL.h>
+using namespace std;
 
 char c = 0;
-
 static struct termios old, new2;
+void initTermios(int echo);
+void resetTermios(void);
+char getch_(int echo);
+char getch(void);
+char getche(void);
 
-/* Initialize new terminal i/o settings */
-void initTermios(int echo) 
+namespace patch
 {
-	tcgetattr(0, &old); /* grab old terminal i/o settings */
-	new2 = old; /* make new settings same as old settings */
-	new2.c_lflag &= ~ICANON; /* disable buffered i/o */
-	new2.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
-	tcsetattr(0, TCSANOW, &new2); /* use these new terminal i/o settings now */
+    template <typename T> std::string to_string(const T& str)
+    {
+        std::ostringstream stm;
+        stm << str;
+        return stm.str();
+    }
 }
 
-/* Restore old terminal i/o settings */
-void resetTermios(void) 
-{
-	tcsetattr(0, TCSANOW, &old);
-}
-
-/* Read 1 character - echo defines echo mode */
-char getch_(int echo) 
-{
-	char ch;
-	initTermios(echo);
-	ch = getchar();
-	resetTermios();
-	return ch;
-}
-
-/* Read 1 character without echo */
-char getch(void) 
-{
-	return getch_(0);
-}
-
-/* Read 1 character with echo */
-char getche(void) 
-{
- 	return getch_(1);
-}
-
-/**
- * This tutorial demonstrates simple sending of messages over the ROS system.
- */
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "talker");
-	ros::NodeHandle n;
-	/* Tu zmienić nazwę robota! */
-	ros::Publisher chatter_pub = n.advertise<geometry_msgs::Twist>("/pioneer2dx_with_sensors/RosAria/cmd_vel", 10);
+	string robotName = "pioneer2dx";
+	//string robotName = "pioneer2dx_with_sensors";
+  ros::init(argc, argv, "talker");
+  ros::NodeHandle n;
 
+	ros::Publisher chatter_pub = n.advertise<geometry_msgs::Twist>("/" + robotName + "/cmd_vel", 10);
+
+  gazebo_msgs::GetModelState getmodelstate;
+	getmodelstate.request.model_name = robotName;
+	ros::ServiceClient gms_c = n.serviceClient<gazebo_msgs::GetModelState>("/gazebo/get_model_state");
+	//getmodelstate.model_name="pioneer2dx";
+	
+
+  //gms_c.call("pioneer2dx", "world");
+	
+	/*if (gms_c.call(getmodelstate))
+	{
+		puts("get_model_state  service call  failed");
+	}
+	if (gms_c)
+	{
+		puts("get_model_state not found");
+	}*/
 	gazebo_msgs::ModelState modelState;
 	
 	geometry_msgs::Pose pose;
 	geometry_msgs::Twist twist;
+
+	geometry_msgs::Pose poseCURR;
+	geometry_msgs::Twist twistCURR;
   
-	ros::ServiceClient client = n.serviceClient<gazebo_msgs::SetModelState>("/gazebo/set_model_state");
-	gazebo_msgs::SetModelState setmodelstate;
-	gazebo_msgs::ModelState modelstate;
-	modelstate.model_name ="pioneer2dx";
-	modelstate.pose = pose;
-	modelstate.twist = twist;
+  ros::ServiceClient client = n.serviceClient<gazebo_msgs::SetModelState>("/gazebo/set_model_state");
+  gazebo_msgs::SetModelState setmodelstate;
+  gazebo_msgs::ModelState modelstate;
+  modelstate.model_name = robotName;
+	modelstate.pose = poseCURR;
+	modelstate.twist = twistCURR;
 	setmodelstate.request.model_state=modelstate;         
 
-	if (client.call(setmodelstate))
+	/*if (client.call(setmodelstate))
 	{
 		ROS_INFO("Connected!!");
 	}
@@ -83,119 +82,310 @@ int main(int argc, char **argv)
 	{
 		ROS_ERROR("Failed to call service for setmodelstate ");
 		return 1;
-	}
+	}*/
 		
 	bool dirty = false;
-	int skala = 1;
+	double linSpeed = 1;
+	double angSpeed = 2;
 	int ang = 0;
 	int lin = 0;
 
   
 	puts("Wywrotowiec wita");
-	puts("Reading from keyboard");
-	puts("---------------------------");
-	puts("Use WASD to move the robot (Q - exit, F - stop, R - reset do [0, 0], T - morderstwo)");
+  //puts("Reading from keyboard");
+  puts("---------------------------");
+  puts("Use ARROWS to move the robot");
+	puts(" (Q - exit, R - reset do [0, 0], G - remember current state, T - random movement");
+	puts(" To modify speed hold for 0.5s (W or S) or (E or D) ofr lin/ang inc/decrease");
 
-	while(1)
- 	{
+	cKeyboard kb;
+	if (kb.active == false)
+	{
+		puts("Run this node as an administrator - try 'sudo su', 'sudo rosrun' doesn't work')");
+		return 0;
+	}
+  initTermios(false);
+	while (true)
+	{
+		ang = 0;
+		lin = 0;
+		if (kb.getKeyState(KEY_UP) && kb.getKeyState(KEY_LEFT))
+		{
+			ROS_INFO("UP + LEFT");
+			ang = -1;
+			lin = 1;
+		}
+		else if (kb.getKeyState(KEY_UP) && kb.getKeyState(KEY_RIGHT))
+		{
+			ROS_INFO("UP + RIGHT");
+			ang = 1;
+			lin = 1;
+		}
+		else if (kb.getKeyState(KEY_DOWN) && kb.getKeyState(KEY_LEFT))
+		{
+			ROS_INFO("DOWN + LEFT");
+			ang = -1;
+			lin = -1;
+		}
+		else if (kb.getKeyState(KEY_DOWN) && kb.getKeyState(KEY_RIGHT))
+		{
+			ROS_INFO("DOWN + RIGHT");
+			ang = 1;
+			lin = -1;
+		}
+		else if (kb.getKeyState(KEY_UP))
+		{
+			ROS_INFO("UP");
+			lin = 1;
+		}
+		else if (kb.getKeyState(KEY_DOWN))
+		{
+			ROS_INFO("DOWN");
+			lin = -1;
+		}
+		else if (kb.getKeyState(KEY_LEFT))
+		{
+			ROS_INFO("LEFT");
+			ang = -1;
+		}
+		else if (kb.getKeyState(KEY_RIGHT))
+		{
+			ROS_INFO("RIGHT");
+			ang = 1;
+		}
+		else if (kb.getKeyState(KEY_Q))
+		{
+			ROS_INFO("SAY GOODBYE!");
+			//usleep(200000);
+  		resetTermios();
+			//usleep(200000);
+			return 0;
+		}
+		else if (kb.getKeyState(KEY_W))
+		{
+			ROS_INFO("W - hold to increase linear speed");
+			usleep(500000);
+			if (kb.getKeyState(KEY_W))
+			{
+				linSpeed += 0.2;
+				cout << "Changed linear speed to " << linSpeed << " [nieznanych jednostek :<]" << endl;
+			}
+		}
+		else if (kb.getKeyState(KEY_S))
+		{
+			ROS_INFO("S - hold to decrease linear speed");
+			usleep(500000);
+			if (kb.getKeyState(KEY_S))
+			{
+				linSpeed -= 0.2;
+				cout << "Changed linear speed to " << linSpeed << " [nieznanych jednostek :<]" << endl;
+			}
+		}
+		else if (kb.getKeyState(KEY_E))
+		{
+			ROS_INFO("E - hold to increase angular speed");
+			usleep(500000);
+			if (kb.getKeyState(KEY_E))
+			{
+				angSpeed += 0.2;
+				cout << "Changed angular speed to " << angSpeed << " [nieznanych jednostek :<]" << endl;
+			}
+		}
+		else if (kb.getKeyState(KEY_D))
+		{
+			ROS_INFO("D - hold to decrease angular speed");
+			usleep(500000);
+			if (kb.getKeyState(KEY_D))
+			{
+				angSpeed -= 0.2;
+				cout << "Changed angular speed to " << angSpeed << " [nieznanych jednostek :<]" << endl;
+			}
+		}
+		else if (kb.getKeyState(KEY_R))
+		{
+			ROS_INFO("R");
+			try 
+			{
+				client.call(setmodelstate);
+			}
+			catch (...)
+			{
+				ROS_INFO("Failed to call service for set_model_state");
+			}
+		}
+		else if (kb.getKeyState(KEY_T))
+		{
+			ROS_INFO("T - brak funkcjonalnosci");
+		}
+		else if (kb.getKeyState(KEY_G))
+		{
+			ROS_INFO("G - brak fukcjonalnosci");
+		}
+		twist.linear.x = linSpeed * lin;
+		twist.angular.z = angSpeed * ang;
+		chatter_pub.publish(twist);			
+		usleep(100000); // 0.1s
+	}
+  resetTermios();
+	usleep(500000);
+	return 0;
+  
+	// Stara obsuga klawiatury, tak zostawiam dla wlasnego uzytku - 
+	/*while (true)
+  {
+		puts("R");
 		ang = 0;
 		lin = 0;
 		c=getch();
-		switch(c)
-		{
-		case 'a':
-		case 'A':
-			ROS_DEBUG("LEFT");        
-			ang = -1;
-			dirty = true;
-			break;
-		case 'd':
-		case 'D':
-			ROS_DEBUG("RIGHT");
-			ang = 1;
-			dirty = true;
-			break;
-		case 'w':
-		case 'W':
-			ROS_DEBUG("UP");
-			lin = -1;
-			dirty = true;
-			break;
-		case 's':
-		case 'S':
-			ROS_DEBUG("DOWN");
-			lin = 1;
-			dirty = true;
-			break;
-		case 'f':
-		case 'F':
-			ROS_DEBUG("STOP");
-			dirty = true;
-			break;
-		case 'r':
-		case 'R':
-			ROS_DEBUG("RESET");
-			dirty = true;
-			if (client.call(setmodelstate))
-			{
-				ROS_INFO("Reset");
-			}
-			else
-			{
-				ROS_ERROR("Failed to call service for setmodelstate ");
-				return 1;
-			}
-			break;
-		case 't':
-		case 'T':
-			while(true)
-			{
-			lin = 0;
-			ang = 1;
+    switch(c)
+    {
+      case 'a':
+      case 'A':
+				ROS_DEBUG("LEFT");        
+				ang = -1;
+        dirty = true;
+        break;
+      case 'd':
+      case 'D':
+        ROS_DEBUG("RIGHT");
+        ang = 1;
+        dirty = true;
+        break;
+      case 'w':
+      case 'W':
+        ROS_DEBUG("UP");
+        lin = -1;
+        dirty = true;
+        break;
+      case 's':
+      case 'S':
+        ROS_DEBUG("DOWN");
+        lin = 1;
+        dirty = true;
+        break;
+			case 'f':
+      case 'F':
+				ROS_DEBUG("STOP");
+				dirty = true;
+				break;
+			case 'g':
+		  case 'G':
+				ROS_DEBUG("GET_STATE");
+				if (!gms_c.call(getmodelstate))
+				{
+					ROS_INFO("GET_STATE");
 
-			twist.linear.x = skala * lin;
-			twist.angular.z = skala * ang * 5;
-			chatter_pub.publish(twist);
-			usleep(1000000);
+					poseCURR = getmodelstate.response.pose;
+					twistCURR = getmodelstate.response.twist;
+				}
+				else
+				{
+					ROS_ERROR("Failed to call service for getstatemodel");
+					return 1;
+				}
+      case 'r':
+      case 'R':
+        ROS_DEBUG("RESET");
+        dirty = true;
+				if (client.call(setmodelstate))
+				{
+					ROS_INFO("Reset");
+				}
+				else
+				{
+					ROS_ERROR("Failed to call service for setmodelstate ");
+					return 1;
+				}
+        break;
+			case 't':
+			case 'T':
+				while(true)
+				{
+					lin = 0;
+					ang = 1;
+					
+					twist.linear.x = speed * lin;
+					twist.angular.z = speed * ang * 5;
+					chatter_pub.publish(twist);
+					usleep(1000000);
 
-			lin = 1;
-			ang = 0;
+					lin = 1;
+					ang = 0;
 
-			twist.linear.x = skala * lin;
-			twist.angular.z = skala * ang * 5;
-			chatter_pub.publish(twist);	
-			usleep(1000000);	
+		      twist.linear.x = speed * lin;
+					twist.angular.z = speed * ang * 5;
+					chatter_pub.publish(twist);	
+					usleep(1000000);	
 
-			lin = 0;
-			ang = -1;
+					lin = 0;
+					ang = -1;
+					
+					twist.linear.x = speed * lin;
+					twist.angular.z = speed * ang * 5;
+					chatter_pub.publish(twist);
+					usleep(500000);
 
-			twist.linear.x = skala * lin;
-			twist.angular.z = skala * ang * 5;
-			chatter_pub.publish(twist);
-			usleep(500000);
+					lin = 1;
+					ang = 0;
 
-			lin = 1;
-			ang = 0;
+		      twist.linear.x = speed * lin;
+					twist.angular.z = speed * ang * 5;
+					chatter_pub.publish(twist);	
+					usleep(1000000);	
+				}
+			case 'q':
+      case 'Q':
+				ROS_DEBUG("EXIT");	
+        return 0;
+    }
 
-			twist.linear.x = skala * lin;
-			twist.angular.z = skala * ang * 5;
-			chatter_pub.publish(twist);	
-			usleep(1000000);	
-			}
-		case 'q':
-		case 'Q':
-			ROS_DEBUG("EXIT");	
-			return 0;
-		}
-
-		if(dirty ==true)
-		{
-			twist.linear.x = skala * lin;
-			twist.angular.z = skala * ang * 5;
+    if(dirty ==true)
+    {
+			twist.linear.x = speed * lin;
+			twist.angular.z = speed * ang * 5;
 			chatter_pub.publish(twist);			
 
-			dirty=false;
-		}
-	}
-	return 0;
+      dirty=false;
+    }
+  }*/
+
+  return 0;
+}
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo) 
+{
+  tcgetattr(0, &old); /* grab old terminal i/o settings */
+  new2 = old; /* make new settings same as old settings */
+  new2.c_lflag &= ~ICANON; /* disable buffered i/o */
+  new2.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
+  tcsetattr(0, TCSANOW, &new2); /* use these new terminal i/o settings now */
+}
+
+/* Restore old terminal i/o settings */
+void resetTermios(void) 
+{
+  tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo) 
+{
+  char ch;
+  initTermios(echo);
+  ch = getchar();
+  resetTermios();
+  return ch;
+}
+
+/* Read 1 character without echo */
+char getch(void) 
+{
+  return getch_(0);
+}
+
+/* Read 1 character with echo */
+char getche(void) 
+{
+  return getch_(1);
 }
